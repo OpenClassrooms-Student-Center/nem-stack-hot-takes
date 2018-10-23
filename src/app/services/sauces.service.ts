995f0d7feca0dc2620a50b559246964baa396472
willehpeh <will_alexander@me.com>
2018-10-23 19:22:01 +0200

    Improved single sauce page styling

--- src/app/services/sauces.service.ts
+++ src/app/services/sauces.service.ts
@@ -16,7 +16,7 @@ export class SaucesService {
       _id: 'eizomfhazo',
       name: 'The Last Dab',
       manufacturer: 'Heatonist',
-      description: 'Mind-blowingly hot!',
+      description: 'CAUTION! The Last Dab—the hottest sauce on Hot Ones, known for turning guests and fans alike into stuttering, fire-breathing lunatics—just got even crazier. The Last Dab first took the world by storm as the only sauce made with Smokin\' Ed Currie\'s infamous "Pepper X." This XX-rated edition gets its one-two punch of heat from addition of the equally lethal "Chocolate Pepper X." All you need is a dab to light the inferno and experience the Hot Ones tradition.',
       heat: 10,
       likes: 100,
       dislikes: 0,
@@ -29,7 +29,7 @@ export class SaucesService {
       _id: 'oimhoiohmhoih',
       name: 'Los Calientes',
       manufacturer: 'Heatonist',
-      description: 'Nummy nummy nummy',
+      description: 'Hot Ones is the show where celebrities divulge their deepest secrets while eating progressively hotter wings. The middle of the lineup is where hot sauce magic happens—the perfect sweet spot between maximum flavor and pleasing heat. Inspired by our favorite Cali-Mex flavors, Los Calientes surfs over the palate with a punchy, smoky blend...',
       heat: 5,
       likes: 100,
       dislikes: 0,
@@ -42,7 +42,7 @@ export class SaucesService {
       _id: 'oimjoijlhui',
       name: 'Black Garlic',
       manufacturer: 'Bravado Spice Company',
-      description: 'Still pretty freakin\’ hot',
+      description: 'Team Bravado is back at it with an elevated offering where Carolina Reaper meets aged black garlic. The sweetness of the slowly cooked garlic tempers the initial bitter burn of the Reaper, but not for long... This is a biting hot sauce you\'ll want in marinades, sauces, dressings, and on those garlic wings! ',
       heat: 6,
       likes: 100,
       dislikes: 0,
@@ -55,7 +55,7 @@ export class SaucesService {
       _id: 'sildjhv',
       name: 'Smoked Onion',
       manufacturer: 'Butterfly Bakery',
-      description: 'Mind-blowingly hot!',
+      description: 'The makers at Butterfly Bakery smoke Vermont onions with maplewood to mix with red jalapeños for this sweet and tangy sauce. Great on everything from bagels lox & cream cheese to hummus to pork and whatever else you can name. The medium heat level makes it the perfect smoky sauce for anyone!',
       heat: 3,
       likes: 100,
       dislikes: 0,
@@ -68,7 +68,11 @@ export class SaucesService {
       _id: 'eroimfgjlfh',
       name: 'Blair\'s Ultra Death Sauce',
       manufacturer: 'Blair\'s',
-      description: 'Even more mind-blowingly hot, and not very tasty',
+      description: 'Blair\'s Ultra Death has established itself as a bit of a legend within the hot sauce world.\n' +
+        '\n' +
+        'If there\'s one thing that creator Blair Lazar does well it\'s retaining the flavour in his super-hot sauces. They\'ll melt your face off for sure, but despite the extract they still taste damned fine.\n' +
+        '\n' +
+        'Just to emphasise the seriousness of the heat we\'re dealing with here, all Blair\'s super-hot sauces in the Death range now come in a nifty coffin box with his trademark skull keyring attached to the bottle.',
       heat: 9,
       likes: 100,
       dislikes: 0,
