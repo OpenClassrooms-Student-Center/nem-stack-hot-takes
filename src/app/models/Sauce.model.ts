54e901faf3eb6512b657f208e682f7625f848cb9
willehpeh <will_alexander@me.com>
2018-10-23 16:55:50 +0200

    Sauce model added

---
+++ src/app/models/Sauce.model.ts
@@ -0,0 +1,10 @@
+export class Sauce {
+  _id: string;
+  name: string;
+  manufacturer: string;
+  description: string;
+  heat: number;
+  likeScore: number;
+  imageUrl: string;
+  mainPepper: string;
+}
